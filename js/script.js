36cf0f542d78ee81907500ddad6162520eb22d0e
Vinayak Abhang <vinayal.abhang@crayoninfotech.com>
2023-05-17 13:56:20 +0530

    create product details page

--- js/script.js
+++ js/script.js
@@ -122,3 +122,88 @@ $('ul.tabs li').click(function () {
     $(this).addClass('current');
     $("#" + tab_id).addClass('current');
 })
+
+
+$('.slider-single').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    adaptiveHeight: true,
+    infinite: false,
+    useTransform: true,
+    speed: 400,
+    cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+});
+
+$('.slider-nav')
+    .on('init', function (event, slick) {
+        $('.slider-nav .slick-slide.slick-current').addClass('is-active');
+    })
+    .slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: false,
+        focusOnSelect: false,
+        infinite: false,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        }, {
+            breakpoint: 640,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        }, {
+            breakpoint: 420,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        }]
+    });
+
+$('.slider-single').on('afterChange', function (event, slick, currentSlide) {
+    $('.slider-nav').slick('slickGoTo', currentSlide);
+    var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+    $('.slider-nav .slick-slide.is-active').removeClass('is-active');
+    $(currrentNavSlideElem).addClass('is-active');
+});
+
+$('.slider-nav').on('click', '.slick-slide', function (event) {
+    event.preventDefault();
+    var goToSingleSlide = $(this).data('slick-index');
+
+    $('.slider-single').slick('slickGoTo', goToSingleSlide);
+});
+
+// new Drift(document.querySelector('.drift_trigger'), {
+//     paneContainer: document.querySelector('.product_details'),
+//     inlinePane: 769,
+//     inlineOffsetY: -85,
+//     containInline: true,
+//     hoverBoundingBox: true
+// });
+
+var driftAll = document.querySelectorAll('.drift_trigger');
+var pane = document.querySelector('.product_details');
+$(driftAll).each(function (i, el) {
+    let drift = new Drift(
+        el, {
+        // zoomFactor: 1.3,
+        paneContainer: pane,
+        inlinePane: false,
+        handleTouch: false,
+
+        // inlinePane: 769,
+        inlinePane: 375,
+        inlineOffsetY: -85,
+        containInline: true,
+        hoverBoundingBox: true
+    }
+    );
+});
